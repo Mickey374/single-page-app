7484c3497ad00335a796430f775619475ce56ccd
Mickey374 <asantemichael37483@gmail.com>
2023-04-19 00:53:50 +0200

    Added basic bootstrap config to the List

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
 import List from "./components/List";
 
 function App() {
   // return <h1>Rick and Morty</h1>;
   return (
-    <div>
-      <h1>Rick and Mortyy</h1>
+    <div className="container">
+      <nav className="navbar sticky-top navbar-light bg-dark">
+        <h1 className="navbar-brand text-light">Rick and Mortyy</h1>
+      </nav>
       <List />
     </div>
   );
 }
 
 const container = document.getElementById("root");
-// console.log("Container " + container);
 const root = ReactDOM.createRoot(container);
-// console.log("Root " + root);
 root.render(<App />);
